9f6a3a2f795198b21ee90cc55cccfb22b2ed9add
Christian Treppo <christian@treppo.org>
2019-06-26 21:42:44 +0200

    Simplify install script

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -1,16 +1,5 @@
 #!/usr/local/bin/zsh
 
+brew install exa jenv bat prettyping htop neovim
+
 git submodule update --init --recursive
-
-ln -s $HOME/.yadr/nvim/ $HOME/.config/nvim
-ln -s $HOME/.yadr/git/gitconfig $HOME/.gitconfig
-
-ln -s ${ZDOTDIR:-$HOME}/.yadr/zsh/prezto ${ZDOTDIR:-$HOME}/.zprezto
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-
-ln -s $HOME/.yadr/zsh/.zpreztorc $HOME/.zpreztorc
-
