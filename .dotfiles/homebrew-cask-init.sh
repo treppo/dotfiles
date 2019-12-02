703960faa5af3b26e8f16c47e7cae8f75865ec75
Christian Treppo <christian@treppo.org>
2019-12-02 10:28:26 +0100

    Update casks

--- .dotfiles/homebrew-cask-init.sh
+++ .dotfiles/homebrew-cask-init.sh
@@ -4,35 +4,37 @@ brew install caskroom/cask/brew-cask
 
 brew cask install \
   1password \
-  android-file-transfer \
-  appcleaner \
-  atom \
-  # carbon-copy-cloner \
-  chefdk
-  dropbox \
-  entonnoir \
-  epic \
-  firefox \
-  flowdock \
-  flux \
-  gimp \
-  google-chrome \
-  grandperspective \
-  iterm2 \
-  jubler \
-  karabiner \
-  macdown \
-  # manager \
-  mplayerx \
-  nvalt \
-  platypus \
-  skype \
-  spotify \
-  torbrowser \
-  transmission \
-  tunnelblick \
-  vagrant \
-  virtualbox \
-  vlc \
-  qtox
-
+  adoptopenjdk12 \ 
+  docker \ 
+  font-cooper-hewitt \ 
+  font-source-code-pro \ 
+  intellij-idea \ 
+  postman \ 
+  textmate \ 
+  adoptopenjdk8 \ 
+  dotnet-sdk \ 
+  font-fira-code \ 
+  font-source-serif-pro \ 
+  iterm2 \ 
+  signal \ 
+  the-unarchiver \ 
+  android-studio \ 
+  firefox \ 
+  font-fira-mono \ 
+  franz \ 
+  keybase \ 
+  sloth \ 
+  visual-studio-code \ 
+  appcleaner \ 
+  flux \ 
+  font-fira-sans \ 
+  google-drive-file-stream \ 
+  mono-mdk \ 
+  spotify \ 
+  zoomus \ 
+  chromium \ 
+  font-charter \ 
+  font-ibm-plex \ 
+  grandperspective \ 
+  nvalt \ 
+  telegram
