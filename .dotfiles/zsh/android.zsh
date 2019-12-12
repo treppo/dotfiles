db86cb66854403ae083c684f88fda41a9344ab51
Christian Treppo <christian@treppo.org>
2019-12-12 11:00:59 +0100

    Update android paths

--- .dotfiles/zsh/android.zsh
+++ .dotfiles/zsh/android.zsh
@@ -1,4 +1,5 @@
-export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
+export ANDROID_HOME=$ANDROID_SDK_ROOT
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
