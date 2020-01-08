6057e942d01a14172716609cdac740783e32860b
Christian Treppo <christian@treppo.org>
2020-01-08 12:52:58 +0100

    Simplify dotfile structure

--- .dotfiles/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/zsh
-
-brew install exa jenv bat prettyping htop neovim diff-so-fancy fish
-
-chsh -s /usr/local/bin/fish
--- .setup/install.sh
+++ .setup/install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
 brew update
-brew install caskroom/cask/brew-cask
+brew install exa jenv bat prettyping htop neovim diff-so-fancy fish
+
+chsh -s /usr/local/bin/fish
 
 brew cask install \
   1password \
@@ -8,33 +11,30 @@ brew cask install \
   docker \ 
   font-cooper-hewitt \ 
   font-source-code-pro \ 
+  font-fira-mono \ 
+  font-fira-sans \ 
+  font-fira-code \ 
+  font-source-serif-pro \ 
+  font-charter \ 
+  font-ibm-plex \ 
   intellij-idea \ 
   postman \ 
   textmate \ 
   adoptopenjdk8 \ 
-  dotnet-sdk \ 
-  font-fira-code \ 
-  font-source-serif-pro \ 
   iterm2 \ 
   signal \ 
   the-unarchiver \ 
-  android-studio \ 
   firefox \ 
-  font-fira-mono \ 
   franz \ 
   keybase \ 
   sloth \ 
   visual-studio-code \ 
   appcleaner \ 
   flux \ 
-  font-fira-sans \ 
   google-drive-file-stream \ 
-  mono-mdk \ 
   spotify \ 
   zoomus \ 
   chromium \ 
-  font-charter \ 
-  font-ibm-plex \ 
   grandperspective \ 
   nvalt \ 
   telegram
