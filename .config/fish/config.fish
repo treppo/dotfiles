10d75b02ccb7f2c6d9cdea3f09ef55aaa634ae3a
Christian Treppo <christian@treppo.org>
2020-03-17 18:36:11 +0100

    Fix shell paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,5 +7,6 @@ end
 set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 
 source ~/.iterm2_shell_integration.(basename $SHELL)
-set -g fish_user_paths "$HOME/.jenv/bin:$PATH" $fish_user_paths
+
+set -g fish_user_paths "$HOME/.jenv/bin" $fish_user_paths
 status --is-interactive; and source (jenv init -|psub)
