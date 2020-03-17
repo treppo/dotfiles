6739db7d2a2f23d77b660802a03c9b93356b0ce4
Christian Treppo <christian@treppo.org>
2020-03-17 18:37:50 +0100

    Update git alias

--- .config/fish/functions/gnb.fish
+++ .config/fish/functions/gnb.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function gnb
-	git checkout -b
+function gnb --wraps='git checkout --branch' --wraps='git checkout -b' --description 'alias gnb=git checkout -b'
+  git checkout -b $argv;
 end
