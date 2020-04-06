d6f318160bbeac5ae54ba117a8ae2dc0cd874f53
Christian Treppo <christian@treppo.org>
2020-04-06 09:21:14 +0200

    Add homebrew paths to fish config in right order

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,12 +4,15 @@ if not functions -q fisher
     fish -c fisher
 end
 
-set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
-
 source ~/.iterm2_shell_integration.(basename $SHELL)
 
 set -g fish_user_paths "$HOME/.jenv/bin" $fish_user_paths
 status --is-interactive; and source (jenv init -|psub)
+
+# Homebrew paths
+set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 
 # Added by Krypton
 set -x GPG_TTY (tty)
