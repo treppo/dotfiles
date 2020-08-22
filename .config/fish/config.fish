3eee44d4ae4e971a2cdcef56cfba32f427cfa3fe
Christian Treppo <christian@treppo.org>
2020-08-22 17:43:57 +0200

    Use Secretive instead of Krypton

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,5 @@ set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 
-# Added by Krypton
-set -x GPG_TTY (tty)
+# Secretive
+set --export SSH_AUTH_SOCK /Users/treppo/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
