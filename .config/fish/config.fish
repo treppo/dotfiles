ab88db49b0f27c83edf0f1a9a6c597d2d5bf5f6b
Christian Treppo <christian@treppo.org>
2020-08-22 17:41:22 +0200

    Remove nvm and fisher

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,3 @@
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 source ~/.iterm2_shell_integration.(basename $SHELL)
 
 set -g fish_user_paths "$HOME/.jenv/bin" $fish_user_paths
