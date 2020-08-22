94ee416cd6f7f7e6d93d0df6024a63c3155c3311
Christian Treppo <christian@treppo.org>
2020-08-22 17:42:13 +0200

    Remove jenv shell integration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,5 @@
 source ~/.iterm2_shell_integration.(basename $SHELL)
 
-set -g fish_user_paths "$HOME/.jenv/bin" $fish_user_paths
-status --is-interactive; and source (jenv init -|psub)
-
 # Homebrew paths
 set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
