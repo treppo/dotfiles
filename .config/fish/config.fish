9f221f2e76c03da89f3dc4f73a973562aa3f2d65
Christian Treppo <christian@treppo.org>
2020-12-26 14:04:14 +0100

    Add Python bins to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,8 @@ set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 
+# Python paths
+set -g fish_user_paths (python3 -m site --user-base)/bin $fish_user_paths
+
 # Secretive
 set --export SSH_AUTH_SOCK /Users/treppo/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
