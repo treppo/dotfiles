947b261660ce74a590210ef168fb75e82640448e
Christian Treppo <christian@treppo.org>
2021-01-12 13:08:26 +0100

    Switch to Alacritty as terminal

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,3 @@
-source ~/.iterm2_shell_integration.(basename $SHELL)
-
 # Homebrew paths
 set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
