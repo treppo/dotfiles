5d6bd307dbe246a528ca8f5e712aa6e6ebfc80b6
Christian Treppo <christian@treppo.org>
2022-02-24 15:20:26 +0100

    Remove unused fish functions

--- .config/fish/functions/cat.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function cat
-	bat --theme 'GitHub' $argv
-end
--- .config/fish/functions/vim.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function vim --wraps=nvmi --wraps=nvim --description 'alias vim nvim'
-  nvim  $argv;
-end
