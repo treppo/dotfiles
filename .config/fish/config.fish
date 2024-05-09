ea9c58665c64516d8cdf383b893d884eff7c597b
Christian Treppo <christian@treppo.org>
2024-05-09 15:02:18 +0200

    Remove asdf fish configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,9 +5,5 @@ fish_add_path /opt/homebrew/sbin
 # Secretive
 set --export SSH_AUTH_SOCK /Users/treppo/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
 
-# asdf
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
-source ~/.asdf/plugins/java/set-java-home.fish
-
 # IntelliJ
 fish_add_path /Applications/IntelliJ\ IDEA.app/Contents/MacOS
